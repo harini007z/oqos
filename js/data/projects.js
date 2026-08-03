6560a8522f8650edadd8d79e92ff2184ad0caa26
harini007z <58560884+harini007z@users.noreply.github.com>
2026-08-03 09:57:01 +0700

    Add files via upload

---
+++ js/data/projects.js
@@ -0,0 +1,23 @@
+var soixanteprojects = [{
+      url: 'https://www.effectivecpmnetwork.com/b3emvsszs0?key=24ac8f74b6b0941716fbfc37866447b0',
+      type: 'internet'
+}, {
+      url: 'https://eooqz.lovelyglrl.com/?utm_source=da57dc555e50572d&ban=other&j5=1&j6=1&s1=15810&s2=2209620&click_id=diablo',
+      type: 'scenographie'
+}, {
+      url: 'https://www.effectivecpmnetwork.com/b3emvsszs0?key=b827158e5f98395b8020b49450758cf0',
+      type: 'application'
+}, {
+      url: 'https://eooqz.lovelyglrl.com/?utm_source=da57dc555e50572d&ban=other&j5=1&j6=1&s1=15810&s2=2209620&click_id=diablo',
+      type: 'enseignement'
+}, {
+      url: 'https://www.effectivecpmnetwork.com/b3emvsszs0?key=b827158e5f98395b8020b49450758cf0',
+      type: 'scenographie'
+}, {
+      url: 'https://eooqz.lovelyglrl.com/?utm_source=da57dc555e50572d&ban=other&j5=1&j6=1&s1=15810&s2=2209620&click_id=diablo',
+      type: 'internet'
+}
+//{url:''},
+//{url:''},
+//{url:''},
+];
